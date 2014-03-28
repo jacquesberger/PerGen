f6949742bd47bce0b4e6a92f537fc93cdb548c6c
Jacques Berger <jacques.berger@code3.ca>
2014-03-28 00:55:26 -0400

    Exemple d'injection de dépendance présenté en classe.

--- src/main/java/org/jberger/pergen/generators/JavaGenerator.java
+++ src/main/java/org/jberger/pergen/generators/JavaGenerator.java
@@ -1,18 +1,17 @@
 /* Copyright 2007 Jacques Berger
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package org.jberger.pergen.generators;
 
 import java.io.File;
@@ -33,16 +32,16 @@ import org.jberger.pergen.codeproviders.Java6Provider;
 public final class JavaGenerator {
 
     String directory;
-    
+
     public JavaGenerator(String dir) {
         directory = dir;
     }
-    
+
     public void generate(final DataLayerSpecifications global) throws IOException {
         generateDAOs(global);
         generatePOJOs(global);
     }
-    
+
     /**
      * Generates the DAOException.java file.
      */
@@ -91,50 +90,51 @@ public final class JavaGenerator {
     /**
      * Generates the POJOs (Plain-Old Java Object) corresponding to the
      * entities.
+     *
      * @param global All the entities.
      */
-    private void generatePOJOs(final DataLayerSpecifications global) {
+    private void generatePOJOs(final DataLayerSpecifications global) throws IOException {
         String realDirectory = directory + "\\pojos";
         new File(realDirectory).mkdir();
 
         for (Entity entity : global.getEntities()) {
             String fileName = realDirectory + "\\" + entity.getJavaName()
-                              + ".java";
-            try {
-                FileWriter fileWriter = new FileWriter(fileName);
-                FileWriterWrapper writer = new FileWriterWrapper(fileWriter);
-                Java6Provider.provideHeaderComment(writer);
-                Java6Provider.providePackageDeclaration(writer, "pojos");
-                Java6Provider.providePOJOsImports(writer, entity);
-                Java6Provider.provideClassDeclaration(writer,
-                                                      entity.getJavaName());
-                Java6Provider.provideDefaultConstructor(writer,
-                                                        entity.getJavaName());
+                    + ".java";
+            FileWriter fileWriter = new FileWriter(fileName);
+            FileWriterWrapper writer = new FileWriterWrapper(fileWriter);
+            generatePojoFile(entity, writer);
+            fileWriter.close();
+        }
+    }
 
-                Java6Provider.providePOJOsIdGetterSetter(writer);
-                for (Field field : entity.getFields()) {
-                    Java6Provider.providePOJOsFieldAndGetterSetter(writer,
-                                                                   field);
-                }
+    private void generatePojoFile(Entity entity, FileWriterWrapper writer) throws IOException {
+        Java6Provider.provideHeaderComment(writer);
+        Java6Provider.providePackageDeclaration(writer, "pojos");
+        Java6Provider.providePOJOsImports(writer, entity);
+        Java6Provider.provideClassDeclaration(writer,
+                entity.getJavaName());
+        Java6Provider.provideDefaultConstructor(writer,
+                entity.getJavaName());
 
-                for (Relation relation : entity.getRelations()) {
-                    if (relation.getType() == RelationType.Type.ONE) {
-                        Java6Provider.providePOJOsEverythingForAOneRelation(
-                                            writer,
-                                            relation.getEntity().getJavaName());
-                    } else {
-                        Java6Provider.providePOJOsEverythingForAManyRelation(
-                                            writer,
-                                            relation.getEntity().getJavaName());
-                    }
-                }
+        Java6Provider.providePOJOsIdGetterSetter(writer);
+        for (Field field : entity.getFields()) {
+            Java6Provider.providePOJOsFieldAndGetterSetter(writer,
+                    field);
+        }
 
-                Java6Provider.provideClassEnd(writer);
-                fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+        for (Relation relation : entity.getRelations()) {
+            if (relation.getType() == RelationType.Type.ONE) {
+                Java6Provider.providePOJOsEverythingForAOneRelation(
+                        writer,
+                        relation.getEntity().getJavaName());
+            } else {
+                Java6Provider.providePOJOsEverythingForAManyRelation(
+                        writer,
+                        relation.getEntity().getJavaName());
             }
         }
+
+        Java6Provider.provideClassEnd(writer);
     }
 
     private void generateDaoJavaCode(FileWriterWrapper writer, Entity entity, String className) throws IOException {
